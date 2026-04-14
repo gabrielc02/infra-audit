8d120a9c3c6a3166fb67db3cda99ecc120551e94
gabrielc02 <gabriel.clsilva@usp.br>
2026-04-14 20:14:07 -0300

    feat: add json file reader

--- internal/io/file.go
+++ internal/io/file.go
@@ -1,1 +1,25 @@
 package io
+
+import (
+	"encoding/json"
+	"fmt"
+	"infra-audit/internal/inventory"
+	"os"
+)
+
+func Reader() ([]inventory.Asset, error) {
+
+	file, err := os.ReadFile("internal/inventory/inventory.json")
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir arquivo: %w", err)
+	}
+
+	var assets []inventory.Asset
+
+	err = json.Unmarshal(file, &assets)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao converter JSON: %w", err)
+	}
+
+	return assets, nil
+}
