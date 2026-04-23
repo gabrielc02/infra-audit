d097714101699018b8b5e149dcd8715a830925af
gabrielc02 <gabriel.clsilva@usp.br>
2026-04-23 07:59:46 -0300

    feat: implement main function

--- cmd/infra-audit/main.go
+++ cmd/infra-audit/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"infra-audit/internal/io"
+	"infra-audit/internal/report"
+)
+
 func main() {
+	assets, err := io.Reader()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	reported := report.Report(assets)
+
+	for _, rep := range reported {
+		fmt.Printf("REPORTED{%v}", rep)
+	}
 
 }
